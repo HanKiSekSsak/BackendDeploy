68aad204cb82edad063824b3bd72a771e6db83b6
Jhun24 <jjanghuni102@naver.com>
2022-07-11 02:46:52 +0900

    init repository

---
+++ static/js/category.js
@@ -0,0 +1,48 @@
+let secretForm = document.getElementById('secret-form');
+let category = document.getElementById('form-category');
+let listBox = document.getElementById('list-box');
+let categoryInput = document.getElementById('category');
+
+const SearchList = ['찌개', '파스타', '밥', '찜'];
+
+let SubmitList = (e)=>{
+    category.value = e.target.innerHTML;
+    secretForm.submit();
+}
+
+let CheckSearch = (e)=>{
+    let text = e.target.value;
+
+    let appendList = [];
+
+    console.log(text)
+
+    if(text != ""){    
+        for(let i = 0; i<SearchList.length; i++){
+            console.log(SearchList[i].includes(text));  
+            if(SearchList[i].includes(text) == true){
+                appendList.push(SearchList[i]);
+            }
+        }
+
+        let html = ""
+        console.log(appendList)
+
+        if(appendList.length != 0){
+            for(let i = 0; i<appendList.length; i++){
+                html += '<div class="list" onclick="SubmitList(event)">'+appendList[i]+'</div>'
+            }
+    
+        }
+        listBox.innerHTML = html;
+    }
+    else{
+        
+        let html = ""
+        for(let i = 0; i<SearchList.length; i++){
+            html += '<div class="list" onclick="SubmitList(event)">'+SearchList[i]+'</div>'
+        }
+
+        listBox.innerHTML = html;
+    }
+}  
---
+++ tag_list/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render, redirect
+from .forms import categoryform, foodform
+from django.contrib import auth
+from django.shortcuts import get_object_or_404
+from .models import food
+from django.contrib.auth.models import User
+
+def mobile(request):
+    return render(request, 'mobile.html')
+
+# Create your views here.
+def home(request):
+    global user
+    user = request.user
+    foodname = food.objects.filter(user=user)
+    return render(request, 'main.html', {'user':user,'foodname':foodname})
+
+def category(request):
+    user = request.user
+    foodname = food.objects.filter(user=user)
+    return render(request, 'category.html',{'foodname':foodname})
+
+def ingredient(request):
+    global user
+    user = food.objects.filter(user = request.user.id)
+    return render(request, 'list.html', {'user':user})
+
+def barcode(request):
+    if request.method == 'POST':
+        user = request.user
+        foodname = request.POST.get('foodname')
+        date = request.POST.get('date')
+        foodf = foodform({
+            "user":user,
+            "foodname":foodname,
+            "date":date
+        })
+        if foodf.is_valid():
+            foodf.save()
+            return redirect('home')
+    else:
+        foodf = foodform()
+    return render(request, 'barcode.html')
+
+def select(request):
+    return render(request, 'select_recipe.html')
+
+def recipe(request):
+    recipe_name = request.POST.get('recipe')
+    return render(request, 'recipe.html', {'recipe_name' : recipe_name})
+
+def jjigae(request):
+    return render(request, 'jjigae.html')
+
+def soup(request):
+    return render(request, 'soup.html')
+
+def steam(request):
+    return render(request, 'steam.html')
+
+def roast(request):
+    return render(request, 'roast.html')
+
+def tang(request):
+    return render(request, 'tang.html')
